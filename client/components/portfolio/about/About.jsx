b78dffca5343fde6ef741722d2eb106cbbbb4954
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-10-13 12:12:00 +1300

    Added in temp content to pages

--- client/components/portfolio/about/About.jsx
+++ client/components/portfolio/about/About.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Link to="/"><h3>⬅ PJS</h3></Link>
+            <h1>About me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                officia deserunt mollit anim id est laborum.
+            </p>
+        </>
     )
 }
