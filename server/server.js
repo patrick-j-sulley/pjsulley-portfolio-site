60ee8381d7be7e0f3f803e3fdefa153d4749fea4
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-10-25 22:49:07 +1300

    Heroku deployment attempt #3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 3000
 const server = express()
 
 server.use(express.json())
@@ -9,7 +8,5 @@ server.use(express.static(__dirname + '/public'))
 server.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
-
-server.listen(port)
 
 module.exports = server
